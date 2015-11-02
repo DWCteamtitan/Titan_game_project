122f8dcfcb1c0c99e439c4dd9f00572dc127f7cd
camarello <camarello@student.dwc.edu>
2015-11-02 17:42:43 -0500

    Main Menu 2.0
    Added more Updates to Main Menu

---
+++ Assets/Main Menu/LoadLvl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLvl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Load () {
+		Application.LoadLevel(1); //1 was the level 1 design
+		//Application.LoadLevel(3); //3 is the invidual scene for movie
+		Time.timeScale = 1f;
+		
+	}
+	public void Quit(){
+		Application.Quit ();
+	}
+
+	public void Menu(){
+		Application.LoadLevel(0);
+	}
+
+	public void Credits()
+	{
+		Application.LoadLevel (2);}
+
+	public void Movie()
+	{
+		Application.LoadLevel(3); //3 is the invidual scene for movie
+		Time.timeScale = 1f;
+	}
+}
