2b30261e4119b7342b734b3e7cee35d3c07861be
Shermanftw <shermannator@gmail.com>
2015-11-03 19:31:09 -0500

    Prototype Main menu test start
    Main Menu

---
+++ Assets/camera_menu_look.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_menu_look : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
