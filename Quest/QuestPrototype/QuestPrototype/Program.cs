b526a8faf86cae193a30849dfab43e911a65a77b
Chrisl007 <lafond.chris@yahoo.com>
2015-11-10 11:00:12 -0500

    QuestLogMaker_Proto
    This is a prototype and is not ready for use/release. If anyone wants to
    edit this go ahead but I doubt any of you will want to hahaha! This is
    also a test to see if I can make a programming branch separate from the
    main that I can work on!

---
+++ Quest/QuestPrototype/QuestPrototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace QuestPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument doc = XDocument.Load(@"K:\Capstone\Titan_game_project\Assets\XmlTest.xml");
+            XElement Item = doc.Element("Item");
+            Item.Add(new XElement("Student",
+           new XElement("FirstName", "David"),
+           new XElement("LastName", "Smith")));
+            doc.Save(@"K:\Capstone\Titan_game_project\Assets\XmlTest.xml");
+        }
+
+        
+    }
+
+
+}
