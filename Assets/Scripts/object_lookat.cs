20b3def050eeacd912adb11c0ce94f95061feac2
camarello <camarello@student.dwc.edu>
2015-11-10 21:06:02 -0500

    Would you look at that?
    Object LookAt Script. Facing anything

---
+++ Assets/Scripts/object_lookat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class object_lookat : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt (target);
+	}
+}
